44b3bc04183ccc493d66f5d46de08f668ea066a3
Rob Garrison <wowmotty@gmail.com>
2010-08-26 12:43:29 -0500

    cleaned up & made easily resizable

--- README.markdown
+++ README.markdown
@@ -1,15 +1,44 @@
-### Usage
+### Usage & Options
 
-	$('.slider').movingBoxes({ startPanel: 3 });
-	
-### Keyboard Navigation
+	$('.slider').movingBoxes({
+		startPanel  : 1,                      // start with this panel
+		width       : 800,                    // overall width of movingBoxes
+		panelWidth  : .5,                     // current panel width adjusted to 50% of movingBoxes width
+		reducedSize : .8,                     // non-current panel size: 80% of current panel size
+		imageRatio  : 4/3,                    // Image ratio set to 4:3
+		speed       : 500,                    // animation time in milliseconds
+		leftArrow   : 'images/leftarrow.png', // left arrow image url
+		rightArrow  : 'images/rightarrow.png' // right arrow image url
+	});
 
-There is a file in the /js/ folder called `slider-noplugin-withkeyboard.js` which is the original non-plugin
-version of this. It's not as easy to have multiple sliders on the page this way, but it does include 
-keyboard navigation. If anyone feels like working on this, moving the keyboard nav into the plugin would 
-be awesome.
+### Methods
+- Uses standard index (starts from one)
+- Get:	var panel = $('.slider').data('movingBoxes').currentPanel();  // returns # of currently selected/enlarged panel
+- Set:	var panel = $('.slider').data('movingBoxes').currentPanel(2); // returns # and scrolls to 2nd panel
 
 ### Credits
 
 - Original Script by Chris Coyier
-- Modified into [a plugin](http://wowmotty.blogspot.com/2010/06/moving-boxes-updated.html) by Rob (aka Fudgey)
+- Modified into [a plugin](http://wowmotty.blogspot.com/2010/06/moving-boxes-updated.html) by Rob G (aka Mottie)
+
+### Changelog
+
+- Version 1.4 (8/26/2010) Rob Garrison
+-- Reduced the amount of HTML markup - Internalized with CSS adjustment of elements (e.g. arrow images)
+-- Added left and right navigation arrow urls to the options. The images are added as <img> by the script in an attempt to reduce the amount of required HTML markup
+-- Removed sizing options (movingDistance, curWidth, curImgWidth, curTitleSize, curParSize).
+-- Added options to set overall width (width), panelWidth (50% of overall width), reducedSize (80% of currently displayed panel), imageRation (4:3 ratio to resize images to properly fit the panel).
+-- Added keyboard support for multiple sliders (added back arrow keys and spacebar; but not the enter key as it will follow the external links)
+-- Added panel centering to fix [issue #2](http://github.com/chriscoyier/MovingBoxes/issues#issue/2).
+-- Updated the index.html to give examples of different movingBox sizes and image ratios.
+
+- Version 1.3 (6/21/2010) Rob Garrison
+-- Significant rewrite & conversion to a plugin (based off of http://starter.pixelgraphics.us/)
+-- Removed keyboard support, it would look wierd having mulitple panels moving. Maybe someone has a better method?
+-- Added method to get/set current displayed panel
+- Version 1.2 (2/17/2009) Noah Hendrix
+-- Keyboard support added, arrow keys, spacebar, and enter key
+-- Code cleaned up, number of panels and initial widths/sizes are no longer hard coded, so easier to build upon
+-- Clicking on non-active left or right panels also triggers animation
+- Version 1.1 (2/16/2009) Chris Coyier - Issue with double clicking fixed. Next animation can only start when current animation is complete.
+- Version 1.0 (2/16/2009) Chris Coyier - Script released to public
--- js/jquery.movingboxes.js
+++ js/jquery.movingboxes.js
@@ -2,21 +2,10 @@
  * Moving Boxes script by Chris Coyier
  * http://css-tricks.com/moving-boxes/
  *
- * Revision History:
- * (6/21/2010) Rob Garrison
-    ~ Significant rewrite & conversion to a plugin (based off of http://starter.pixelgraphics.us/)
-    ~ Removed keyboard support, it would look wierd having mulitple panels moving. Maybe someone has a better method?
-    ~ Added method to get/set current displayed panel
- * (2/17/2009) Noah Hendrix
-    ~ Keyboard support added, arrow keys, spacebar, and enter key
-    ~ Code cleaned up, number of panels and initial widths/sizes are no longer hard coded, so easier to build upon
-    ~ Clicking on non-active left or right panels also triggers animation
- * (2/16/2009) Chris Coyier - Issue with double clicking fixed. Next animation can only start when current animation is complete.
- * (2/16/2009) Chris Coyier - Script released to public
  */
 
-(function ($) {
-    $.movingBoxes = function (el, options) {
+(function($){
+    $.movingBoxes = function(el, options){
         // To avoid scope issues, use 'base' instead of 'this'
         // to reference this class from internal events and functions.
         var base = this;
@@ -28,100 +17,177 @@
         // Add a reverse reference to the DOM object
         base.$el.data('movingBoxes', base);
 
-        base.init = function () {
+        base.init = function(){
             base.options = $.extend({}, $.movingBoxes.defaultOptions, options);
 
-            // Put your initialization code here
-            var panels = base.$el.find('.panel');
+            // Setup formatting (to reduce the amount of initial HTML)
+            base.$el
+             .addClass('left-shadow')
+             .css({
+                position : 'relative',
+                left     : 0,
+                top      : 0,
+                width    : base.options.width // override css width
+             })
+             .wrapInner('<div class="scrollContainer" />')
+             .wrapInner('<div class="scroll right-shadow" />')
+             .prepend('<img class="scrollButtons left" src="' + base.options.leftArrow + '" />')
+             .append('<img class="scrollButtons right" src="' + base.options.rightArrow + '" />')
+             .find('.panel').wrapInner('<div class="inside" />').end()
+             .find('.scroll, .scrollContainer').css({
+                overflow : 'hidden',
+                width    : '100%'
+             });
 
-            base.options.totalPanels = panels.length;
-            base.options.regWidth = panels.css('width');
-            base.options.regImgWidth = panels.find('img').css('width');
-            base.options.regTitleSize = panels.find('h2').css('font-size');
-            base.options.regParSize = panels.find('p').css('font-size');
+            // Set up panes & content sizes
+            base.panels = base.$el.find('.panel');
+            base.panels.css({
+                width  : base.options.width * base.options.panelWidth, // default: panelWidth = 50% of entire width
+                float  : 'left'
+            });
+            base.totalPanels = base.panels.length;
 
-            panels.css({
-                'float': 'left',
-                'position': 'relative'
+            // save 'cur' numbers (current larger panel size)
+            base.curWidth = base.panels.outerWidth();
+            base.curImgWidth = parseInt( base.panels.find('img').css('width'), 10);
+            base.curImgHeight = base.curImgWidth / base.options.imageRatio; // set images fit a 4:3 ratio
+            base.curTitleSize = parseInt( base.panels.find('h2').css('font-size'), 10 );
+            base.curParSize = parseInt( base.panels.find('p').css('font-size'), 10 );
+
+            // save 'reg' (reduced size) numbers
+            base.regWidth = base.curWidth * base.options.reducedSize;
+            base.regImgWidth = base.curImgWidth * base.options.reducedSize;
+            base.regImgHeight = base.curImgHeight * base.options.reducedSize;
+            base.regTitleSize = base.curTitleSize * base.options.reducedSize;
+            base.regParSize = base.curParSize * base.options.reducedSize;
+
+            // set image heights so scrollContainer height is correctly set
+            base.panels.find('img').css({ height: base.curImgHeight + 'px' });
+
+            // make scrollContainer wide enough to contain all the panels
+            base.container = base.$el.find('.scrollContainer').css({
+                position : 'relative',
+                width    : (base.curWidth + 50) * base.totalPanels,
+                height   : base.panels.outerHeight(true)
             });
 
-            base.$el.data('currentlyMoving', false);
-
-            base.$el.find('.scrollContainer').css('width', (base.$el.find('.panel')[0].offsetWidth * base.$el.find('.panel').length) + 150);
-
-            base.$el.find('.scroll').css('overflow', 'hidden');
-
             // Set up "Current" panel
-            base.$el.data('currentPanel', -1);
-            base.change(base.options.startPanel);
-            curPanel = base.options.startPanel;
+            base.initialized = false;
+            base.currentlyMoving = false;
+            base.curPanel = 1;
+            base.change(1);
+
+            // animate to chosen start panel - starting from the first panel makes it look better
+            setTimeout(function(){ base.change(base.options.startPanel); }, base.options.speed * 2 );
+            base.initialized = true;
+
+            $(window).load(function(){
+                // position the scroll buttons after the images are loaded
+                base.$el.find('.scrollButtons').css({
+                    top : (base.$el.innerHeight() - base.$el.find('.scrollButtons').height())/2 + 'px'
+                });
+            });
 
             // Set up click on left/right arrows
             base.$el.find('.right').click(function(){
-                base.change(base.$el.data('currentPanel') + 1);
+                base.change(base.curPanel + 1);
             }).end().find('.left').click(function(){
-                base.change(base.$el.data('currentPanel') - 1);
+                base.change(base.curPanel - 1);
             });
 
             // go to clicked panel
-            panels.click(function () {
-                base.change( panels.index($(this)) + 1 );
+            base.panels.click(function(){
+                base.change( base.panels.index($(this)) + 1 );
             });
+
+            // Activate moving box on click or when an internal link obtains focus
+            base.$el.click(function(){
+                base.active($(this));
+            }).find('a').focus(function(){
+                // focused link makes moving box active
+                base.active($(this).closest('.slider'));
+                // focused link centered in moving box
+                var index = $(this).closest('.slider').find('.panel').index($(this).closest('.panel')) + 1;
+                base.change(index);
+            });
+
+            // Add keyboard navigation
+            $(window).keyup(function(e){
+                switch (e.which) {
+                    case 39: case 32: // right arrow & space
+                        if (base.$el.is('.active-slider')){
+                            base.change(base.curPanel + 1);
+                        }
+                        break;
+                    case 37: // left arrow
+                        if (base.$el.is('.active-slider')){
+                            base.change(base.curPanel - 1);
+                        }
+                        break;
+                }
+            });
+
         };
 
         // Resize panels to normal
-        base.returnToNormal = function(num) {
-            base.$el.find('.panel').not(':eq(' + (num-1) + ')')
-                .animate({ width: base.options.regWidth }, base.options.speed)
-                .find('img').animate({ width: base.options.regImgWidth }, base.options.speed).end()
-                .find('h2').animate({ fontSize: base.options.regTitleSize }, base.options.speed).end()
-                .find('p').animate({ fontSize: base.options.regParSize }, base.options.speed);
+        base.returnToNormal = function(num){
+            base.panels.not(':eq(' + (num-1) + ')')
+                .animate({ width: base.regWidth + 'px' }, base.options.speed)
+                .find('img').animate({ width: base.regImgWidth + 'px', height: base.regImgHeight + 'px' }, base.options.speed).end()
+                .find('h2').animate({ fontSize: base.regTitleSize + 'px' }, base.options.speed).end()
+                .find('p').animate({ fontSize: base.regParSize + 'px' }, base.options.speed);
         };
 
         // Zoom in on selected panel
-        base.growBigger = function (num) {
-            base.$el.find('.panel').eq(num-1)
-                .animate({ width: base.options.curWidth }, base.options.speed)
-                .find('img').animate({ width: base.options.curImgWidth }, base.options.speed).end()
-                .find('h2').animate({ fontSize: base.options.curTitleSize }, base.options.speed).end()
-                .find('p').animate({ fontSize: base.options.curParSize }, base.options.speed);
+        base.growBigger = function(num){
+            base.panels.eq(num-1)
+                .animate({ width: base.curWidth + 'px' }, base.options.speed)
+                .find('img').animate({ width: base.curImgWidth + 'px', height: base.curImgHeight + 'px' }, base.options.speed).end()
+                .find('h2').animate({ fontSize: base.curTitleSize + 'px' }, base.options.speed).end()
+                // give focus to link after the panel is in view, or it will change the scrollLeft value
+                .find('p').animate({ fontSize: base.curParSize + 'px' }, base.options.speed, function(){ base.panels.eq(num-1).find('a').focus(); });
         };
 
         // Change view to display selected panel
-        base.change = function(curPanel) {
-
-            var panels = base.$el.find('.panel');
+        base.change = function(curPanel){
             //if not in range or on the same panel then return
-            if (curPanel < 1 || base.$el.data('currentPanel') == curPanel || curPanel > panels.length) {
+            if (base.initialized && (curPanel < 1 || base.curPanel == curPanel || curPanel > base.panels.length)) {
                 return false;
             }
-
-            //if not currently moving
-            if (!base.$el.data('currentlyMoving')) {
-
-                base.$el.data('currentPanel', curPanel);
-                base.$el.data('currentlyMoving', true);
+            // abort if panel is already animating
+            if (!base.currentlyMoving) {
+                base.currentlyMoving = true;
                 // center panel in scroll window
-                var leftValue = base.$el.find('.scroll').innerWidth() / 2 - panels.eq(curPanel-1).outerWidth() / 2;
-                var movement = leftValue - base.options.movingDistance * (curPanel-1);
-                base.$el.find('.scrollContainer').stop().animate({
-                    'left' : movement
-                }, base.options.speed, function () {
-                    base.$el.data('currentlyMoving', false);
-                });
 
+                var leftValue = (base.options.width - base.curWidth) / 2 - base.panels.eq(curPanel-1).position().left;
+                // when scrolling right, add the difference of the larger current panel width
+                if (curPanel > base.curPanel) { leftValue += ( base.curWidth - base.regWidth ); }
+
+                base.curPanel = curPanel;
+                base.$el.find('.scroll').scrollLeft(0); // when links get focus, they shift the scrollLeft if not visible
+                base.container.stop().animate({
+                    left : leftValue
+                }, base.options.speed, function(){
+                    base.currentlyMoving = false;
+                });
                 base.returnToNormal(curPanel);
                 base.growBigger(curPanel);
             }
         };
 
+        // Make moving box active (for keyboard navigation)
+        base.active = function(el){
+            $('.active-slider').removeClass('active-slider');
+            el.addClass('active-slider');
+        };
+
         // get: var currentPanel = $('.slider').data('movingBoxes').currentPanel(); // returns # of currently selected/enlarged panel
         // set: var currentPanel = $('.slider').data('movingBoxes').currentPanel(2); // returns and scrolls to 2nd panel
-        base.currentPanel = function (panel) {
+        base.currentPanel = function(panel){
             if (typeof(panel) !== 'undefined') {
                 base.change(parseInt(panel,10)); // parse in case someone sends a string
             }
-            return base.$el.data('currentPanel');
+            return base.curPanel;
         };
 
         // Run initializer
@@ -129,24 +195,25 @@
     };
 
     $.movingBoxes.defaultOptions = {
-        startPanel: 1,        // start with this panel
-        movingDistance: 300,  // panel distance to move
-        curWidth: 350,        // panel width
-        curImgWidth: 326,     // Image width
-        curTitleSize: '20px', // Title font size
-        curParSize: '15px',   // paragraph font size
-        speed: 500            // time in milliseconds
+        startPanel  : 1,   // start with this panel
+        width       : 800, // overall width of movingBoxes
+        panelWidth  : 0.5, // current panel width adjusted to 50% of overall width
+        reducedSize : 0.8, // non-current panel size: 80% of panel size
+        imageRatio  : 4/3, // Image ratio set to 4:3
+        speed       : 500, // animation time in milliseconds
+        leftArrow   : 'images/leftarrow.png',
+        rightArrow  : 'images/rightarrow.png'
     };
 
-    $.fn.movingBoxes = function (options) {
-        return this.each(function () {
+    $.fn.movingBoxes = function(options){
+        return this.each(function(){
             (new $.movingBoxes(this, options));
         });
     };
 
     // This function breaks the chain, but returns
     // the movingBoxes if it has been attached to the object.
-    $.fn.getmovingBoxes = function () {
+    $.fn.getmovingBoxes = function(){
         this.data('movingBoxes');
     };
 
